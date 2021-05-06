141792c26a29f482040ded893ca14bd377dc639d
Whobuilder <onurkimilli@gmail.com>
2021-05-06 15:16:07 +0200

    Use the right macro variable

--- include/OnKiAlgo/geo2d/line.hpp
+++ include/OnKiAlgo/geo2d/line.hpp
@@ -1,8 +1,8 @@
 #ifndef ONKIALGO_GEO2D_LINE_H
 #define ONKIALGO_GEO2D_LINE_H
-#define POINT2D_CONCEPT(point)                                     \
-    static_assert(std::is_same_v<decltype(p1.x), decltype(p1.y)>); \
-    static_assert(std::is_arithmetic_v<decltype(p1.x)>)
+#define POINT2D_CONCEPT(point)                                           \
+    static_assert(std::is_same_v<decltype(point.x), decltype(point.y)>); \
+    static_assert(std::is_arithmetic_v<decltype(point.x)>)
 
 #include <type_traits>
 
