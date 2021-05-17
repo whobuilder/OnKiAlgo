24a7c55724827200616c9ba775652e7d62fc0321
Whobuilder <onurkimilli@gmail.com>
2021-05-17 12:04:30 +0200

    Add bring_to_front algorithm

---
+++ include/OnKiAlgo/bring_to_front.hpp
@@ -0,0 +1,34 @@
+#ifndef ONKIALGO_BRING_TO_FRONT_H
+#define ONKIALGO_BRING_TO_FRONT_H
+
+#include <algorithm>
+
+namespace onkialgo {
+template<typename ForwardIt, typename T>
+ForwardIt bring_to_front(ForwardIt first, ForwardIt last, const T &value)
+{
+    std::size_t i = 0;
+    while (true) {
+        auto it = std::find(std::next(first, i), last, value);
+        if (it == last) break;
+
+        std::rotate(std::next(first, i), it, last);
+        ++i;
+    }
+    return std::next(first, i);
+}
+
+template<typename ForwardIt, typename UnaryFunction>
+ForwardIt bring_to_front_if(ForwardIt first, ForwardIt last, UnaryFunction &&pred)
+{
+    std::size_t i = 0;
+    while (true) {
+        auto it = std::find_if(std::next(first, i), last, std::forward<UnaryFunction>(pred));
+        if (it == last) break;
+        std::rotate(std::next(first, i), it, last);
+        ++i;
+    }
+    return std::next(first, i);
+}
+}// namespace onkialgo
+#endif /* ONKIALGO_BRING_TO_FRONT_H */
---
+++ test/bring_to_front_tests.cpp
@@ -0,0 +1,39 @@
+#include <catch2/catch.hpp>
+#include <OnKiAlgo/bring_to_front.hpp>
+#include <vector>
+#include <algorithm>
+
+TEST_CASE("bring to front standard test", "[bring_to_front]")
+{
+    std::vector<int> vec{ 1, 2, 3, 4, 5, 2, 3, 2 };
+    int front_value = 2;
+    auto it = onkialgo::bring_to_front(begin(vec), end(vec), front_value);
+    REQUIRE(*it != front_value);
+    REQUIRE(std::all_of(begin(vec), it, [front_value](const auto &val) { return val == front_value; }));
+}
+
+
+TEST_CASE("bring to front non existing value", "[bring_to_front]")
+{
+    std::vector<int> vec{ 1, 2, 3, 4, 5, 2, 3, 2 };
+    int front_value = 42;
+    auto it = onkialgo::bring_to_front(begin(vec), end(vec), front_value);
+    REQUIRE(it == begin(vec));
+}
+
+TEST_CASE("bring to front if standard test", "[bring_to_front, bring_to_front_if]")
+{
+    std::vector<int> vec{ 1, 2, 3, 4, 5, 2, 3, 2 };
+    int front_value = 2;
+    auto it = onkialgo::bring_to_front_if(begin(vec), end(vec), [front_value](int value) { return value == front_value; });
+    REQUIRE(*it != front_value);
+    REQUIRE(std::all_of(begin(vec), it, [front_value](const auto &val) { return val == front_value; }));
+}
+
+TEST_CASE("bring to front if non existing value", "[bring_to_front, bring_to_front_if]")
+{
+    std::vector<int> vec{ 1, 2, 3, 4, 5, 2, 3, 2 };
+    int front_value = 42;
+    auto it = onkialgo::bring_to_front_if(begin(vec), end(vec), [front_value](int value) { return value == front_value; });
+    REQUIRE(it == begin(vec));
+}
