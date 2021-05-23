6cc80fb4116c87c116fb022ed5dfe7128bd183c1
Whobuilder <onurkimilli@gmail.com>
2021-05-23 17:48:41 +0200

    Add is_raw_or_std_array type trait

--- include/OnKiAlgo/is_std_array.hpp
+++ include/OnKiAlgo/is_std_array.hpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <type_traits>
 namespace onkialgo {
+
 template<class T>
 struct is_std_array : std::false_type
 {
@@ -16,6 +17,13 @@ struct is_std_array<std::array<T, N>> : std::true_type
 template<class T>
 inline constexpr bool is_std_array_v = is_std_array<T>::value;
 
+template<typename T>
+struct is_raw_or_std_array : std::conditional_t<(std::is_array_v<T> || is_std_array_v<T>), std::true_type, std::false_type>
+{
+};
+
+template<class T>
+inline constexpr bool is_raw_or_std_array_v = is_raw_or_std_array<T>::value;
 
 }// namespace onkialgo
 
