46661d46ce8f73df1e2fdf1a6f35ed7b0050e103
Whobuilder <onurkimilli@gmail.com>
2021-06-04 21:31:28 +0200

    Use remove_cvref helper

--- include/OnKiAlgo/slice.hpp
+++ include/OnKiAlgo/slice.hpp
@@ -3,11 +3,12 @@
 #include <cstddef>
 #include <algorithm>
 #include <OnKiGenerics/container_factory.hpp>
+#include <OnKiGenerics/remove_cvref.hpp>
 namespace onkialgo {
 template<typename ResultType, typename Container>
 ResultType slice(Container &&c, std::size_t start_index, std ::size_t end_index)
 {
-    using SourceValueType = typename onkigenerics::ContainerFactory<std::remove_cv_t<std::remove_reference_t<Container>>>::value_type;
+    using SourceValueType = typename onkigenerics::ContainerFactory<onkigenerics::remove_cvref_t<Container>>::value_type;
     using SlicedValueType = typename onkigenerics::ContainerFactory<ResultType>::value_type;
     static_assert(std::is_same_v<SourceValueType, SlicedValueType>, "Source and sliced container must have the same value_type!");
     auto size = end_index - start_index;
